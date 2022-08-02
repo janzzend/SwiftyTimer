751b598bf63e15fd7637104c2b3e05e8a025dae7
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2022-08-02 17:13:28 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-	name: "SwiftyTimer",
-	dependencies: []
+    name: "SwiftyTimer",
+    platforms: [.iOS(.v9), .tvOS(.v9)],
+    products: [
+        .library(name: "SwiftyTimer", targets: ["SwiftyTimer"])
+    ],
+    targets: [
+        .target(name: "SwiftyTimer")
+    ]
 )
