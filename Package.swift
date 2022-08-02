5235aa94bc05fa5781d660436f1a0ed97b69abb6
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2022-08-02 17:09:32 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,5 @@ import PackageDescription
 
 let package = Package(
 	name: "SwiftyTimer",
-	dependencies: [],
-    exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]
-    
+	dependencies: []
 )
